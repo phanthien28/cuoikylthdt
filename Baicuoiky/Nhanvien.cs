78a694c84f50749e746c707ddc9d5d163c7f271b
chiendoan123 <97337386+chiendoan123@users.noreply.github.com>
2022-12-20 16:28:34 +0700

    Update Nhanvien.cs

--- Baicuoiky/Nhanvien.cs
+++ Baicuoiky/Nhanvien.cs
@@ -4,7 +4,45 @@ using System.Text;
 
 namespace Baicuoiky
 {
-    class Nhanvien
+    interface Inhanvien
     {
+        int Idnv { get; set; }
+        string hoten { get; set; }
+        int tuoi { get; set; }        
+        string chucvu { get; set; }
+        string phutrach { get; set; }
+        void hienthi();
+        void nhap();
+    }
+
+    class Nhanvien : Inhanvien
+    {
+        public int Idnv { get; set; }
+        public string hoten { get; set; }
+        public int tuoi { get; set; }
+        public string chucvu { get; set; }
+        public string phutrach { get; set; }
+
+        public void hienthi()
+        {
+            Console.Write("Nhap id nhan vien: ");
+            Idnv = int.Parse(Console.ReadLine());
+            Console.Write("Nhap ho va ten nhan vien: ");
+            hoten = Console.ReadLine();
+            Console.Write("Nhap tuoi nhan vien: ");
+            tuoi = int.Parse(Console.ReadLine());
+            Console.Write("Nhap chuc vu nhan vien: ");
+            chucvu = Console.ReadLine();
+            Console.Write("Nhap phu trach nhan vien: ");
+            phutrach = Console.ReadLine();
+        }
+        public void nhap()
+        {
+            Console.WriteLine("ID nhan vien: {0}", Idnv);
+            Console.WriteLine("Ho va ten nhan vien: {0}", hoten);
+            Console.WriteLine("Tuoi nhan vien: {0}", tuoi);
+            Console.WriteLine("Chuc vu nhan vien: {0}", chucvu);
+            Console.WriteLine("Phu trach nhan vien: {0}", phutrach);
+        }
     }
 }
