bdefd9137ac7fca9f8170befe388ceb1dcf21489
chiendoan123 <97337386+chiendoan123@users.noreply.github.com>
2022-12-20 16:34:57 +0700

    Update QLNV.cs

--- Baicuoiky/QLNV.cs
+++ Baicuoiky/QLNV.cs
@@ -6,5 +6,31 @@ namespace Baicuoiky
 {
     class QLNV : Nhanvien
     {
+    List<Nhanvien> nv = new List<Nhanvien>();
+        public void Hienthinv()
+        {
+            foreach (Nhanvien a in nv)
+            {
+                Console.WriteLine("ID nhan vien: {0}", Idnv);
+                Console.WriteLine("Ho va ten nhan vien: {0}", hoten);
+                Console.WriteLine("Tuoi nhan vien: {0}", tuoi);
+                Console.WriteLine("Chuc vu nhan vien: {0}", chucvu);
+                Console.WriteLine("Phu trach nhan vien: {0}", phutrach);
+            }
+        }
+        public void Nhapnv()
+        {
+            string trl;
+            Nhanvien NV;
+            while (true)
+            {
+                NV = new Nhanvien();
+                NV.nhap();
+                nv.Add(NV);
+                Console.Write("Ban muon nhap tiep thi nhan (y): ");
+                trl = Console.ReadLine();
+                if (trl != "y") break;
+            }
+        }
     }
 }
