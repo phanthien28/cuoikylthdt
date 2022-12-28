c21842f9c1da6268f18bf3999053c6a4ddef1109
phanthien28 <113197464+phanthien28@users.noreply.github.com>
2022-12-28 16:32:50 +0700

    Add files via upload

--- Baicuoiky/QLThu.cs
+++ Baicuoiky/QLThu.cs
@@ -1,47 +1,92 @@
-﻿using System;
+﻿using ManagerZoo___Final_Exam.Model;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace Baicuoiky
+namespace ManagerZoo___Final_Exam.Manager
 {
-    class QLThu : Dongvat
+    class QLThu
     {
-        List<Dongvat> dv = new List<Dongvat>();
-        public void Hienthithu()
+        public static List<Thu> listthu = new List<Thu>();
+
+        public static Thu checkThu(String IDthu)
         {
-            foreach (Dongvat a in dv)
+            foreach(Thu t in listthu)
             {
-                Console.WriteLine("Id thu: {0}", Idthu);
-                Console.WriteLine("Loai thu: {0}", Loai);
-                Console.WriteLine("Ten thu: {0}", Ten);
-                Console.WriteLine("Tuoi thu: {0}", tuoi);
-                Console.WriteLine("Ngay nuoi thu: {0}", Ngaynuoi);
-                Console.WriteLine("Nguon goc thu: {0}", Nguongoc);
-                Console.WriteLine("Suc khoe thu: {0}", Suckhoe);
-                Console.WriteLine("Cach di chuyen thu: {0}", Dichuyen);
+                if(t.IDThu1.Equals(IDthu))
+                {
+                    return t;
+                }    
+            }
+            return null;
+        }
+
+        public static void add(String IDChuong)
+        {
+            String LoaiThu;
+            String IDThu;
+            String Ten;
+            String NgayNuoi;
+            String NguonGoc;
+            String SucKhoe;
+            Thu t;
+            do
+            {
+                Console.Write("ID Thu: ");
+                IDThu = Console.ReadLine();
+                t = QLThu.checkThu(IDThu);
+                if (t != null)
+                {
+                    Console.WriteLine("ID Thu da ton tai !!!");
+                    Console.WriteLine("Vui long nhap lai !!!");
+                }
+            } while (t != null);
+            Console.WriteLine("Loai Thu: ");
+            LoaiThu = Console.ReadLine();
+            Console.WriteLine("Ten Thu: ");
+            Ten = Console.ReadLine();
+            Console.WriteLine("Ngay Nuoi: ");
+            NgayNuoi = Console.ReadLine();
+            Console.WriteLine("Nguon Goc: ");
+            NguonGoc = Console.ReadLine();
+            Console.WriteLine("Tinh trang suc khoe: ");
+            SucKhoe = Console.ReadLine();
+            t = new Thu(IDThu, LoaiThu, Ten, IDChuong, NgayNuoi, NguonGoc, SucKhoe);
+            listthu.Add(t);
+            QLChuong.checkChuong(IDChuong).ListThu.Add(t);
+        }
+
+        public static void delete(Chuong chuong)
+        {
+            Console.Write("Nhap ID Thu: ");
+            String IDThu = Console.ReadLine();
+            Thu t;
+            t = QLThu.checkThu(IDThu);
+            if (t == null)
+            {
+                Console.WriteLine("Khong tim thay thu !!!");
+            }
+            else
+            {
+                listthu.Remove(t);
+                chuong.ListThu.Remove(t);
             }
         }
-        public void Nhapthu()
+
+        public static void hienthi(Chuong chuong)
         {
-            string trl;
-            Dongvat dvat;
-            while (true)
+            int check = 0;
+            Console.WriteLine("Chuong {0}", chuong.TenChuong1);
+            Console.WriteLine("ID Thu\t|\tLoai Thu\t|\tTen Thu\t|\tNgay nuoi\t|\tNguon goc\t|\tSuc khoe\t");
+            foreach(Thu t in chuong.ListThu)
             {
-                dvat = new Dongvat();
-                dvat.nhap();
-                dv.Add(dvat);
-                Console.Write("Ban muon nhap tiep thi nhan (y): ");
-                trl = Console.ReadLine();
-                if (trl != "y") break;
+                check++;
+                Console.WriteLine("{0}\t|\t{1}\t|\t{2}\t|\t{3}\t|\t{4}\t|\t{5}\t", t.IDThu1, t.LoaiThu1, t.Ten1, t.NgayNuoi1, t.NgayNuoi1, t.SucKhoe1);
             }
+            if(check == 0)
+            {
+                Console.WriteLine("Khong co con thu nay trong chuong !!!");
+            }    
         }
-        public void Sapxepthu()
-        {
-            dv.Sort(new Sapxepthu());
-        }
-        /*public void timkiem()
-        {
-            dv.Sort(new Timkiemthu());
-        }*/
     }
 }
